8e5c572d8651c950e03be6ae9d101df6803a4dc4
JB Silva Dev <jbsilva.dev@outlook.com>
2023-11-15 14:25:49 -0300

    incluindo episodios

--- filme/admin.py
+++ filme/admin.py
@@ -1,9 +1,28 @@
 from django.contrib import admin
-from .models import Filme
+from .models import Episodio, Filme
+
 
 # Register your models here.
-class ExibicaoAdmin(admin.ModelAdmin):
-    list_display = ("id", "titulo", "categoria", "data_criacao" )
-    list_display_links = ("id", "titulo")
+class ExibicaoAdminFilme(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "titulo",
+        "categoria",
+        "data_criacao",
+    )
+    list_display_links = (
+        "id",
+        "titulo",
+    )
 
-admin.site.register(Filme, ExibicaoAdmin)
+
+class ExibicaoAdminEps(admin.ModelAdmin):
+    list_display = (
+        "titulo",
+        "filme",
+    )
+    list_filter = ("filme",)
+
+
+admin.site.register(Filme, ExibicaoAdminFilme)
+admin.site.register(Episodio, ExibicaoAdminEps)
