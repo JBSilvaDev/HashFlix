eb9415d1aa153c834f837df504a229d7fd0e3451
JB Silva <jbsilva.dev@outlook.com>
2024-01-01 17:00:03 -0300

    criando e estilizando carrossel

--- filme/urls.py
+++ filme/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from .views import *
 
 # Nome passado no arquivo de url padrão no paramentro namespace (opcional) usado quando há urls com mesmo nome para diferenciar a que app pertence
-app_name = 'filme'
+app_name = "filme"
 
 urlpatterns = [
-    path("", HomePage.as_view(), name='homepage'),
-    path("filmes/", HomeFilmes.as_view(), name='homeFilmes'),
-    path("filmes/<int:pk>", DetalhesFilme.as_view(), name='datalhesFilme'),
+    path("", HomePage.as_view(), name="homepage"),
+    path("filmes/", HomeFilmes.as_view(), name="homeFilmes"),
+    path("filmes/<int:pk>", DetalhesFilme.as_view(), name="detalhesFilme"),
 ]
