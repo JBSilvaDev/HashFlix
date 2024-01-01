6f000d09d7240888d48d47abb0f2a00df7682225
JB Silva <jbsilva.dev@outlook.com>
2024-01-01 12:34:23 -0300

    listando filmes relacionados

--- filme/views.py
+++ filme/views.py
@@ -28,8 +28,12 @@ class DetalhesFilme(DetailView):
     # Modelo que sera listado no HTML ( passa como nome object que contem 1 item do nosso modelo )
     model = Filme
 
+    # Função que configura o context
     def get_context_data(self, **kwargs):
+        # Executa o context original da classe, garantindo seu funcionamento
         context = super(DetalhesFilme, self).get_context_data(**kwargs)
-        filmes_relacionados = Filme.objects.filter(categoria=self.get_object().categoria)[0:5]
-        context['filmes_relacionados']=filmes_relacionados
+        # Busca nos filmes categorias compativeis com o filme selecionado (object) self.get_objetc() tras os dados com objetc atual, no exemplo dados de categoria
+        filmes_relacionados = Filme.objects.filter(categoria=self.get_object().categoria)[ 0:5]  # Exibe no maxio 5 filmes com categorias iguais
+        # Adiciona nova chave/valor ao dict context, passando os filmes relacionados para o html dentro da variavel object
+        context["filmes_relacionados"] = filmes_relacionados
         return context
